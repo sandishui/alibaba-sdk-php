e6a4fef80c2087518d94959065e851d4e3e66157
lmz <lmz@lmzdeMacBook-Air.local>
2018-11-15 14:52:43 +0800

    add program

---
+++ CheckProtocol/CheckProtocol.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lmz
+ * Date: 2018/11/14
+ * Time: 下午2:24
+ */
+
+function getHttpCode($url) {
+    // init url
+    $curl = curl_init();
+    // setting url
+    curl_setopt($curl, CURLOPT_URL, $url);
+    // no output to the screen
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    // start execute
+    $xx = curl_exec($curl);
+
+    // get http status code
+    $return = curl_getinfo($curl, CURLINFO_HTTP_CODE); //我知道HTTPSTAT码哦～
+
+    // finished closed
+    curl_close($curl);
+
+    return  $return;
+
+}
+
+function  checkHttpCode($code){
+    if ($code == 0){
+        echo  "error: ",$code;
+    }else{
+        echo  $code;
+    }
+}
+
+
+$code =  getHttpCode("https://huodong.aliyuncs.com");
+
+
+checkHttpCode($code);
+
+?>
